96aa1fc2ca02336ad961ec5b33837bcf38a3a8e1
temaantonov11 <temaantonov11@yandex.ru>
2023-10-22 22:19:25 +0300

    add IBlock

--- TetrisGame.py
+++ TetrisGame.py
@@ -20,7 +20,7 @@ pygame.display.set_caption("Tetris Game")
 clock = pygame.time.Clock()
 #создаем сетку
 game_grid = Grid()
-block = JBlock()
+block = IBlock()
 # Рендеринг
 screen.fill(BLACK)
 # после отрисовки всего, переворачиваем экран
--- blocks.py
+++ blocks.py
@@ -11,6 +11,7 @@ class LBlock(Block):
             3: [Position(0, 0), Position(0, 1), Position(1, 1), Position(2, 1)]
         }
 
+
 class JBlock(Block):
     def __init__(self):
         super().__init__(id=2)
@@ -19,4 +20,15 @@ class JBlock(Block):
             1: [Position(0, 1), Position(0, 2), Position(1, 1), Position(2, 1)],
             2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 2)],
             3: [Position(0, 1), Position(1, 1), Position(2, 0), Position(2, 1)]
+        }
+
+
+class IBlock(Block):
+    def __init__(self):
+        super().__init__(id=3)
+        self.cells = {
+            0: [Position(1, 0), Position(1, 1), Position(1, 2), Position(1, 3)],
+            1: [Position(0, 2), Position(1, 2), Position(2, 2), Position(3, 2)],
+            2: [Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)],
+            3: [Position(0, 1), Position(1, 1), Position(2, 1), Position(3, 1)]
         }
