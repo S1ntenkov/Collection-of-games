51572454a75b8ec41c0e0b2b096bcfcf37797a44
temaantonov11 <temaantonov11@yandex.ru>
2023-10-22 22:39:22 +0300

    add ZBlock

--- blocks.py
+++ blocks.py
@@ -65,3 +65,14 @@ class TBlock(Block):
             2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 1)],
             3: [Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 1)]
         }
+
+
+class ZBlock(Block):
+    def __init__(self):
+        super().__init__(id=7)
+        self.cells = {
+            0: [Position(0, 0), Position(0, 1), Position(1, 1), Position(1, 2)],
+            1: [Position(0, 2), Position(1, 1), Position(1, 2), Position(2, 1)],
+            2: [Position(1, 0), Position(1, 1), Position(2, 1), Position(2, 2)],
+            3: [Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 0)]
+        }
