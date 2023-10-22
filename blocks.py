c7e6b2a1a31ed374c6ea9fe239818834da2b7abe
temaantonov11 <temaantonov11@yandex.ru>
2023-10-22 22:14:01 +0300

    add JBlock

--- blocks.py
+++ blocks.py
@@ -3,10 +3,20 @@ from position import Position
 
 class LBlock(Block):
     def __init__(self):
-        super().__init__(id = 1)
+        super().__init__(id=1)
         self.cells = {
-            0: [Position(0, 2), Position(1, 0), Position(1, 1), Position(1,2)],
-            1: [Position(0,1), Position(1,1), Position(2, 1), Position(2,2)],
-            2: [Position(1,0), Position(1,1), Position(1,2), Position(2,0)],
-            3: [Position(0,0), Position(0, 1), Position(1, 1), Position(2, 1)]
+            0: [Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2)],
+            1: [Position(0, 1), Position(1, 1), Position(2, 1), Position(2, 2)],
+            2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 0)],
+            3: [Position(0, 0), Position(0, 1), Position(1, 1), Position(2, 1)]
+        }
+
+class JBlock(Block):
+    def __init__(self):
+        super().__init__(id=2)
+        self.cells = {
+            0: [Position(0, 0), Position(1, 0), Position(1, 1), Position(1, 2)],
+            1: [Position(0, 1), Position(0, 2), Position(1, 1), Position(2, 1)],
+            2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 2)],
+            3: [Position(0, 1), Position(1, 1), Position(2, 0), Position(2, 1)]
         }
