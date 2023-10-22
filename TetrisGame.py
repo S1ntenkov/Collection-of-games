5ed58d4444a874023fcdbe22d7e2ad71cb9e1bb2
Tim Samoilov <tssamoilov@edu.hse.ru>
2023-10-22 20:34:58 +0300

    first comm

--- TetrisGame.py
+++ TetrisGame.py
@@ -1,33 +1,32 @@
-import pygame
+import pygame, sys
 import random
+from grid import Grid
 
-WIDTH = 500
-HEIGHT = 700
-FPS = 30
-# Задаем цвета
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+WIDTH = 400
+HEIGHT = 800
+FPS = 45
+DARK_BLUE = (44, 44, 100)
 # Создаем игру и окно
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tetris Game")
 clock = pygame.time.Clock()
-# Рендеринг
-screen.fill(BLACK)
-# после отрисовки всего, переворачиваем экран
-pygame.display.flip()
+
 # Цикл игры
 running = True
+game_grid = Grid()
+game_grid.print_grid()
+
 while running:
-    # Держим цикл на правильной скорости
+    pygame.display.update()
     clock.tick(FPS)
     # Ввод процесса (события)
     for event in pygame.event.get():
         # check for closing window
         if event.type == pygame.QUIT:
             running = False
+    screen.fill(DARK_BLUE)
+    
 pygame.quit()
+sys.exit()
---
+++ grid.py
@@ -0,0 +1,24 @@
+class Grid:
+    def __init__(self):
+        self.num_rows = 20
+        self.num_cols = 10
+        self.cell_size = 40
+        self.grid = [[0 for j in range(self.num_cols)] for i in range(self.num_rows)]
+        self.colors = self.get_cell_colors()
+    
+    def print_grid(self):
+        for row in range(self.num_rows):
+            for column in range(self.num_cols):
+                print(self.grid[row][column], end =  " ")
+            print()
+    
+    def get_cell_colors(self):
+        dark_grey = (26, 31, 40)
+        green = (47, 230, 23)
+        red = (232, 18, 18)
+        orange = (226, 116, 17)
+        yellow = (237, 234, 4)
+        purple = (166, 0, 247)
+        cyan = (21, 204, 209)
+        blue = (13, 64, 216)
+        return [dark_grey, green, red, orange, yellow, purple, cyan, blue]
