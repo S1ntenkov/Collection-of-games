c0a137a5b46fb23cf078bbd7fbd710ff9d1cf113
Egor <crafter13cool@gmail.com>
2023-10-23 17:39:51 +0300

    added new texture of apple and redesigned game area

--- SnakeGame.py
+++ SnakeGame.py
@@ -6,16 +6,17 @@ from pygame.math import Vector2
 
 pygame.init()
 #pygame.mixer.init()
+rect1 = pygame.Rect((20,20, 380, 400))
 
 class SNAKE:
     def __init__(self):
-        self.body = [Vector2(5,10),Vector2(6,10),Vector2(7,10)]
+        self.body = [Vector2(6,10),Vector2(7,10),Vector2(8,10)]
         self.direction = Vector2(1,0)
     def draw_snake(self):
 
         for block in self.body:
             block_rect = pygame.Rect(block.x*cell_size,block.y*cell_size,cell_size,cell_size)
-            pygame.draw.rect(screen, ("green"), block_rect)
+            pygame.draw.rect(screen, ("white"), block_rect)
         #fruit_rect = pygame.Rect(self.body ,cell_size, cell_size*2)
     def move_snake(self):
         body_copy = self.body[:-1]
@@ -25,20 +26,25 @@ class SNAKE:
 
 class FRUIT:
     def __init__(self):
-        self.x= random.randint(0,cell_number-1)
-        self.y= random.randint(0,cell_number-1)
+        self.x= random.randint(1,cell_number-1)
+        self.y= random.randint(1,cell_number-1)
         self.pos = (Vector2(self.x*cell_size,self.y*cell_size))
     def draw_fruit(self):
         fruit_rect = pygame.Rect(self.pos.x,self.pos.y,cell_size,cell_size)
-        pygame.draw.rect(screen,("red"),fruit_rect)
+        #pygame.draw.rect(screen,apple,fruit_rect)
+        screen.blit(apple, fruit_rect)
 
-cell_size = 40
+#cell_size = 40
+#cell_number = 20
+cell_size = 20
 cell_number = 20
 
-screen = pygame.display.set_mode((cell_size*cell_number, cell_size*cell_number))
+#screen = pygame.display.set_mode((cell_size*cell_number, cell_number*cell_size))
+screen = pygame.display.set_mode((420,500))
 pygame.display.set_caption("Snake")
 clock = pygame.time.Clock()
-
+#Текстура яблока
+apple= pygame.image.load('текстура-яблока.png').convert()
 
 pygame.display.flip()
 
@@ -52,8 +58,8 @@ pygame.time.set_timer(SCREEN_UPDATE,150)
 end=1
 player_score = "0"
 
-font = pygame.font.SysFont(None, 24)
-img = font.render(player_score, True, "blue")
+font = pygame.font.SysFont('Unispace', 80)
+img = font.render(player_score, True, "white")
 
 while running:
     for event in pygame.event.get():
@@ -74,15 +80,15 @@ while running:
 
 
         if snake.body[0] == fruit.pos/cell_size:
-            fruit.pos=Vector2(random.randint(0,cell_number-1)*cell_size,random.randint(0,cell_number-1)*cell_size)
+            fruit.pos=Vector2(random.randint(1,cell_number-1)*cell_size,random.randint(1,cell_number-1)*cell_size)
             snake.body.append(Vector2(fruit.pos/cell_size))
             print("ate an apple")
             end+=1
             player_score=str(int(player_score)+1)
-            img = font.render(player_score, True, "blue")
+            img = font.render(player_score, True, "white")
 
 
-        if snake.body[0][0]<0 or snake.body[0][0]>19 or snake.body[0][1]<0 or snake.body[0][1]>19:
+        if snake.body[0][0]<1 or snake.body[0][0]>19 or snake.body[0][1]<1 or snake.body[0][1]>20:
             running=False
 
         for body in snake.body[3:]:
@@ -95,10 +101,14 @@ while running:
 
 
 
-    screen.fill((255, 215, 70))
-    screen.blit(img, (20, 20))
+
+    #screen.fill((255, 215, 70))
+    screen.fill((0,0,0))
+    screen.blit(img, (30, 430))
     fruit.draw_fruit()
     snake.draw_snake()
+    pygame.draw.rect(screen, ((255, 255, 255)), rect1, 2)
+
 
 
     #print([fruit.x,fruit.y])
